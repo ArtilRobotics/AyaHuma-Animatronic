900cf70cf1b1afad8a598f721c8bc4ce01dbd159
StevenCuripallo98 <stevencuripallo@hotmail.com>
2022-04-04 18:45:49 -0500

    Programa para el movimiento atenuado de un servo

---
+++ Movimiento Atenuado.ino
@@ -0,0 +1,20 @@
+#include <Servo.h>
+Servo legright;
+
+int switch1 = 0;
+float switch1Smoothed;
+float switch1Prev=90;
+
+void setup() {
+  // put your setup code here, to run once:
+  legright.attach(9);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  switch1Smoothed = (switch1 * 0.05) + (switch1Prev * 0.95);
+  switch1Prev = switch1Smoothed;
+  legright.write(switch1Smoothed);
+  delay(20);
+    
+}
